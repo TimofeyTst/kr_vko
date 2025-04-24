b95c4de46cec222af2f7ea85cc6e6ac557db83ea
John Doe <John@gmail.com>
2025-04-24 18:52:20 +0000

    working

--- kp.sh
+++ kp.sh
@@ -1,44 +1,34 @@
 #!/bin/bash
 
-# Проверка на запуск от root
-if [[ $EUID -eq 0 ]]; then
-	echo "Ошибка: Запуск от root запрещен!"
-	exit 1
-fi
+# Проверка условий запуска
+[[ $EUID -eq 0 ]] && { echo "Запуск от root запрещен!"; exit 1; }
+[[ "$(uname)" != "Linux" ]] && { echo "Скрипт поддерживается только в Linux!"; exit 1; }
+[[ -z "$BASH_VERSION" ]] && { echo "Скрипт должен выполняться в Bash!"; exit 1; }
 
-# Проверка ОС
-if [[ "$(uname)" != "Linux" ]]; then
-	echo "Ошибка: Скрипт поддерживается только в Linux!"
-	exit 1
-fi
+ROOT_DIR=$(dirname "$(realpath "$0")")
+DB_PATH="$ROOT_DIR/db/vkr.db"
 
-# Проверка оболочки
-if [[ -z "$BASH_VERSION" ]]; then
-	echo "Ошибка: Скрипт должен выполняться в Bash!"
-	exit 1
-fi
-
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-DB_DIR="$SCRIPT_DIR/db"
-DB_FILE="$DB_DIR/vko.db"
-MESSAGES_DIR="$SCRIPT_DIR/messages"
+MESSAGES_DIR="$ROOT_DIR/messages"
 DETECTIONS_DIR="$MESSAGES_DIR/detections"
 SHOOTING_DIR="$MESSAGES_DIR/shooting"
 AMMO_DIR="$MESSAGES_DIR/ammo"
 CHECK_DIR="$MESSAGES_DIR/check"
 
 # Определяем папку для логов
-KP_LOG="$SCRIPT_DIR/logs/kp_log.txt"
->"$KP_LOG" # Очистка файла при запуске
+KP_LOG_PATH="$ROOT_DIR/logs/kp.log"
+>"$KP_LOG_PATH" # Очистка файла при запуске
+
+# Создание необходимых директорий
+mkdir -p "$MESSAGES_DIR" "$DETECTIONS_DIR" "$SHOOTING_DIR" "$AMMO_DIR" "$CHECK_DIR" &>/dev/null
 
 # Создание базы данных и таблиц, если они не существуют
-initialize_database() {
-	if [[ -f "$DB_FILE" ]]; then
-		echo "База данных существует, удаляем"
-		rm -f "$DB_FILE"
+init_database() {
+	if [[ -f "$DB_PATH" ]]; then
+		echo "Database already exist, delete it"
+		rm -f "$DB_PATH"
 	fi
 
-	sqlite3 "$DB_FILE" <<EOF
+	sqlite3 "$DB_PATH" <<EOF
     CREATE TABLE IF NOT EXISTS targets (
         id TEXT PRIMARY KEY,
         speed REAL,
@@ -83,132 +73,100 @@ initialize_database() {
 EOF
 }
 
-# Функция для расшифровки и проверки файла
-decrypt_and_verify_message() {
-	local file_path="$1"
-	local file_content=$(<"$file_path")
+# Расшифровка и проверка сообщения
+verify_and_decrypt() {
+    local file="$1"
+    local checksum=$(head -n1 "$file" | cut -d' ' -f1)
+    local encoded=$(cut -d' ' -f2- "$file")
+    local decoded=$(echo -n "$encoded" | base64 -d)
+    local actual_checksum=$(echo -n "$decoded" | sha256sum | cut -d' ' -f1)
 
-	# Извлекаем контрольную сумму и зашифрованный текст
-	local saved_checksum=$(echo "$file_content" | head -n1 | cut -d' ' -f1)
-	local encrypted_content=$(echo "$file_content" | cut -d' ' -f2-)
-
-	# Декодируем обратно в текст
-	local decrypted_content=$(echo -n "$encrypted_content" | base64 -d)
-
-	# Вычисляем хеш и сверяем
-	local calculated_checksum=$(echo -n "$decrypted_content" | sha256sum | cut -d' ' -f1)
-
-	if [ "$saved_checksum" = "$calculated_checksum" ]; then
-		echo "$decrypted_content"
-	else
-		echo "ВНИМАНИЕ!ВНИМАНИЕ!ВНИМАНИЕ! ДАННЫЕ ПОВРЕЖДЕНЫ! ВОЗМОЖЕН НСД!" >&2
-		echo "$(date '+%d-%m %H:%M:%S.%3N') Возможен несанкционированный доступ" >>"$KP_LOG"
-		return 1
-	fi
+    if [[ "$checksum" == "$actual_checksum" ]]; then
+        echo "$decoded"
+    else
+		echo "ALARM: нарушение целостности файла!" >&2
+        echo "$(date '+%Y-%m-%d %H:%M:%S.%3N') ALARM: нарушение целостности файла $file" >>"$KP_LOG_PATH"
+        return 1
+    fi
 }
 
-# Функция для получения ID системы (добавляет в базу, если ее нет)
-get_system_id() {
-	local system_name="$1"
-	local system_num
+# Получение system_id по имени (добавляет в базу, если ее нет)
+upsert_system_id() {
+    local name="$1"
+    local id
+    id=$(sqlite3 "$DB_PATH" "SELECT id FROM systems WHERE name='$name';")
 
-	system_num=$(sqlite3 "$DB_FILE" "SELECT id FROM systems WHERE name='$system_name';")
-
-	if [[ -z "$system_num" ]]; then
-		sqlite3 "$DB_FILE" "INSERT INTO systems (name) VALUES ('$system_name');"
-		system_num=$(sqlite3 "$DB_FILE" "SELECT id FROM systems WHERE name='$system_name';")
-	fi
-
-	echo "$system_num"
+    if [[ -z "$id" ]]; then
+        sqlite3 "$DB_PATH" "INSERT INTO systems (name) VALUES ('$name');"
+        id=$(sqlite3 "$DB_PATH" "SELECT id FROM systems WHERE name='$name';")
+    fi
+    echo "$id"
 }
 
-# Функция обработки файла обнаружений
-process_detection() {
-	local decrypted_content="$1"
-	local file="$2"
+# Обработка обнаружений (расшифрованные данные)
+handle_detection() {
+    local msg="$1" file="$2"
+    read -r ts time system_id target_id x_line y_line speed target_type <<<"$msg"
+    local x=${x_line#*:}
+    local y=${y_line#*:}
+    local dir="NULL"
 
-	timestamp=$(echo "$decrypted_content" | cut -d' ' -f1,2)
-	system_id=$(echo "$decrypted_content" | cut -d' ' -f3)
-	target_id=$(echo "$decrypted_content" | cut -d' ' -f4)
-	x=$(echo "$decrypted_content" | cut -d' ' -f5 | cut -d':' -f2)
-	y=$(echo "$decrypted_content" | cut -d' ' -f6 | cut -d':' -f2)
-	speed=$(echo "$decrypted_content" | cut -d' ' -f7)
-	target_type=$(echo "$decrypted_content" | cut -d' ' -f8-)
-
-	echo "$timestamp $system_id $target_id $x $y $speed $target_type"
+	echo "$ts $time $system_id $target_id $x $y $speed $target_type"
 
 	if [[ "$target_type" == "ББ БР-1" ]]; then
-		direction=1
+		dir=1;
 		target_type="ББ БР"
-		echo "$timestamp $system_id Обнаружена цель ID:$target_id с координатами X:$x Y:$y, скорость: $speed м/с $target_type" >>"$KP_LOG"
-		echo "$timestamp $system_id Цель ID:$target_id движется в сторону СПРО" >>"$KP_LOG"
+    	echo "$ts $time $system_id Обнаружена цель ID:$target_id X:$x Y:$y V:$speed м/с $target_type" >>"$KP_LOG_PATH"
+		echo "$ts $time $system_id Цель ID:$target_id движется к СПРО" >>"$KP_LOG_PATH"
 	else
-		direction="NULL"
-		echo "$timestamp $system_id Обнаружена цель ID:$target_id с координатами X:$x Y:$y, скорость: $speed м/с $target_type" >>"$KP_LOG"
+    	echo "$ts $time $system_id Обнаружена цель ID:$target_id X:$x Y:$y V:$speed м/с $target_type" >>"$KP_LOG_PATH"
 	fi
 
-	sqlite3 "$DB_FILE" "INSERT OR IGNORE INTO targets (id, speed, ttype, direction) VALUES ('$target_id', $speed, '$target_type', $direction);"
+    sqlite3 "$DB_PATH" "INSERT OR IGNORE INTO targets (id, speed, ttype, direction) VALUES ('$target_id', $speed, '$target_type', $dir);"
 
-	sys_id=$(get_system_id "$system_id")
-
-	sqlite3 "$DB_FILE" "INSERT INTO detections (target_id, system_id, x, y, timestamp) VALUES ('$target_id', $sys_id, $x, $y, '$timestamp');"
+    local sys_id=$(upsert_system_id "$system_id")
+	sqlite3 "$DB_PATH" "INSERT INTO detections (target_id, system_id, x, y, timestamp) VALUES ('$target_id', $sys_id, $x, $y, '$timestamp');"
 
 	rm -f "$file"
 }
 
-# Функция обработки файла стрельбы
-process_shooting() {
-	local decrypted_content="$1"
-	local file="$2"
+# Обработка файла стрельбы
+handle_shooting() {
+    local msg="$1" file="$2"
+    read -r ts time system_id target_id result_ts result_time result <<<"$msg"
 
-	timestamp=$(echo "$decrypted_content" | cut -d' ' -f1,2)
-	system_id=$(echo "$decrypted_content" | cut -d' ' -f3)
-	target_id=$(echo "$decrypted_content" | cut -d' ' -f4)
-	result_data=$(echo "$decrypted_content" | cut -d' ' -f5-)
+	echo "$ts $time $system_id $target_id $result_ts $result_time $result"
 
-	echo "$timestamp $system_id $target_id $result_data"
+    local sys_id=$(upsert_system_id "$system_id")
+    if [[ -z "$result_ts" ]]; then
+        sqlite3 "$DB_PATH" "INSERT INTO shooting (target_id, system_id, timestamp) VALUES ('$target_id', $sys_id, '$ts $time');"
+        ((ammo[$system_id]--))
+        echo "$ts $time $system_id Выстрел по ID:$target_id. Боезапас: ${ammo[$system_id]}" >>"$KP_LOG_PATH"
+    else
+        local last_id=$(sqlite3 "$DB_PATH" "SELECT id FROM shooting WHERE target_id='$target_id' AND system_id=$sys_id AND timestamp='$ts $time' ORDER BY id DESC LIMIT 1;")
+        sqlite3 "$DB_PATH" "UPDATE shooting SET result=$result, result_timestamp='$result_ts $result_time' WHERE id=$last_id;"
+        echo "$ts $time $system_id $([[ $result == 1 ]] && echo 'Уничтожена цель' || echo 'Промах') по ID:$target_id при выстреле в $result_ts $result_time " >>"$KP_LOG_PATH"
+    fi
 
-	sys_id=$(get_system_id "$system_id")
-
-	if [[ -z "$result_data" ]]; then
-		sqlite3 "$DB_FILE" "INSERT INTO shooting (target_id, system_id, timestamp) VALUES ('$target_id', $sys_id, '$timestamp');"
-		(( ammo_count[$system_id]-- ))
-		echo "$timestamp $system_id Выстрел по цели ID:$target_id. Осталось снарядов: ${ammo_count[$system_id]}" >>"$KP_LOG"
-	else
-		result_timestamp=$(echo "$result_data" | cut -d' ' -f1,2)
-		result=$(echo "$result_data" | cut -d' ' -f3)
-
-		# Находим последнюю запись с совпадающими system_id, target_id и timestamp
-		last_id=$(sqlite3 "$DB_FILE" "SELECT id FROM shooting WHERE target_id = '$target_id' AND system_id = $sys_id AND timestamp = '$timestamp' ORDER BY id DESC LIMIT 1;")
-
-		sqlite3 "$DB_FILE" "UPDATE shooting SET result = $result, result_timestamp = '$result_timestamp' WHERE id = $last_id;"
-		echo "$result_timestamp $system_id $([[ "$result" == "1" ]] && echo "Уничтожена цель" || echo "Промах по цели") ID:$target_id при выстреле в $timestamp" >>"$KP_LOG"
-	fi
-
-	rm -f "$file"
+    rm -f "$file"
 }
 
-declare -A ammo_count
+declare -A ammo
 
-# Функция обработки пополения боекомплекта
-process_ammo() {
-	local decrypted_content="$1"
-	local file="$2"
+# Пополение боекомплекта
+handle_ammo() {
+    local msg="$1" file="$2"
+    read -r ts time system_id count <<<"$msg"
+    
+	ammo["$system_id"]=$count
 
-	timestamp=$(echo "$decrypted_content" | cut -d' ' -f1,2)
-	system_id=$(echo "$decrypted_content" | cut -d' ' -f3)
-	count=$(echo "$decrypted_content" | cut -d' ' -f4)
+	echo "$ts $time $system_id $count"
+	echo "$ts $time $system_id Боезапас обновлен. Загружено $count снарядов" >>"$KP_LOG_PATH"
 
-	echo "$timestamp $system_id $count"
+	local sys_id=$(upsert_system_id "$system_id")
+    sqlite3 "$DB_PATH" "INSERT INTO ammo (system_id, count, timestamp) VALUES ($sys_id, $count, '$ts $time');"
 
-	echo "$timestamp $system_id Боекомплект обновлен. Загружено $count снарядов" >>"$KP_LOG"
-	ammo_count[$system_id]=$count
-
-	sys_id=$(get_system_id "$system_id")
-
-	sqlite3 "$DB_FILE" "INSERT INTO ammo (system_id, count, timestamp) VALUES ($sys_id, $count, '$timestamp');"
-
-	rm -f "$file"
+    rm -f "$file"
 }
 
 declare -A systems_map=(
@@ -227,10 +185,11 @@ for key in "${!systems_map[@]}"; do
 	system_status["$key"]=1 # 1 - работает
 done
 
-check_systems() {
+health_check() {
 	while true; do
 		for key in "${!systems_map[@]}"; do
 			if [[ ! -f "$CHECK_DIR/ping_$key" ]]; then
+                echo "PING $key"
 				touch "$CHECK_DIR/ping_$key"
 			fi
 		done
@@ -242,16 +201,16 @@ check_systems() {
 				# Если система впервые перестала работать, выводим сообщение
 				if [[ ${system_status[$key]} -eq 1 ]]; then
 					check_time=$(date '+%d-%m %H:%M:%S.%3N')
-					echo "$check_time ${systems_map[$key]} работоспособность потеряна!"
-					echo "$check_time ${systems_map[$key]} работоспособность потеряна!" >>"$KP_LOG"
+					echo "$check_time ${system_names[$key]} is NOT AVAILABLE"
+					echo "$check_time ${system_names[$key]} is NOT AVAILABLE" >>"$KP_LOG_PATH"
 					system_status["$key"]=0 # Отмечаем как неработающую
 				fi
 			else
 				# Если система была неработающей, но теперь отвечает, выводим сообщение о восстановлении
 				if [[ ${system_status[$key]} -eq 0 ]]; then
 					check_time=$(date '+%d-%m %H:%M:%S.%3N')
-					echo "$check_time ${systems_map[$key]} работоспособность восстановлена!"
-					echo "$check_time ${systems_map[$key]} работоспособность восстановлена!" >>"$KP_LOG"
+					echo "$check_time ${system_names[$key]} is AVAILABLE"
+					echo "$check_time ${system_names[$key]} is AVAILABLE" >>"$KP_LOG_PATH"
 					system_status["$key"]=1 # Отмечаем как работающую
 				fi
 			fi
@@ -262,24 +221,22 @@ check_systems() {
 	done
 }
 
-initialize_database
-mkdir -p "$DETECTIONS_DIR" "$SHOOTING_DIR" "$AMMO_DIR" "$CHECK_DIR"
-
-check_systems &
+init_database
+health_check &
 
 echo "Мониторинг файлов в $DETECTIONS_DIR, $SHOOTING_DIR и $AMMO_DIR"
 while true; do
-	mapfile -t files < <(find "$DETECTIONS_DIR" "$SHOOTING_DIR" "$AMMO_DIR" -type f -printf "%T@ %p\n" 2>/dev/null | sort -n | tail -n 10 | cut -d' ' -f2-)
+	mapfile -t recent_files < <(find "$DETECTIONS_DIR" "$SHOOTING_DIR" "$AMMO_DIR" -type f -printf "%T@ %p\n" 2>/dev/null | sort -n | tail -n 10 | cut -d' ' -f2-)
 
-	for file in "${files[@]}"; do
-		decrypted_content=$(decrypt_and_verify_message "$file") || continue
+	for file in "${recent_files[@]}"; do
+		decrypted_message=$(verify_and_decrypt "$file") || continue
 
 		if [[ "$file" == "$DETECTIONS_DIR/"* ]]; then
-			process_detection "$decrypted_content" "$file"
+			handle_detection "$decrypted_message" "$file"
 		elif [[ "$file" == "$SHOOTING_DIR/"* ]]; then
-			process_shooting "$decrypted_content" "$file"
+			handle_shooting "$decrypted_message" "$file"
 		elif [[ "$file" == "$AMMO_DIR/"* ]]; then
-			process_ammo "$decrypted_content" "$file"
+			handle_ammo "$decrypted_message" "$file"
 		fi
 	done
 	sleep 0.01
